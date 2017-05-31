cb5502e9b07d7402dc226207180e6df414f66a79
ydlx002 <469289742@qq.com>
2017-05-31 22:41:24 +0800

    添加菜单管理界面

--- src/main/java/com/ydlx/controller/system/SystemController.java
+++ src/main/java/com/ydlx/controller/system/SystemController.java
@@ -1,28 +1,34 @@
 package com.ydlx.controller.system;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by ydlx on 2017/5/30.
  */
 @Controller
+@RequestMapping(value="/system")
 public class SystemController {
 
 
     @GetMapping(value="/user/index")
+    @PreAuthorize("hasRole('BROWER')")
     public String userIndex(){
         return "/system/user/index";
     }
 
 
     @GetMapping(value="/role/index")
+    @PreAuthorize("hasRole('BROWER')")
     public String roleIndex(){
         return "/system/role/index";
     }
 
 
     @GetMapping(value="/menu/index")
+    @PreAuthorize("hasRole('BROWER')")
     public String menuIndex(){
         return "/system/menu/index";
     }
