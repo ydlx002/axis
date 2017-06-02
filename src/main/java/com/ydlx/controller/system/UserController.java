ae767c49fa704d75ef2526173042e18ef946ad18
ydlx002 <469289742@qq.com>
2017-06-02 22:26:52 +0800

    添加菜单管理界面

--- src/main/java/com/ydlx/controller/system/UserController.java
+++ src/main/java/com/ydlx/controller/system/UserController.java
@@ -1,53 +1,31 @@
 package com.ydlx.controller.system;
 
-import com.ydlx.dao.UserMapper;
 import com.ydlx.domain.info.UserInfo;
 import com.ydlx.domain.vo.PageResultVO;
 import com.ydlx.domain.vo.UserInfoVO;
 import com.ydlx.domain.vo.request.user.QueryUserVO;
-import com.ydlx.utils.DateUtil;
+import com.ydlx.service.system.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by ydlx on 2017/5/30.
  */
 @RestController
-@RequestMapping(value="/user")
+@RequestMapping(value="/system/user")
 public class UserController {
 
     @Autowired
-    private UserMapper userMapper;
+    private UserService userService;
 
-    public PageResultVO<UserInfoVO> getUserList(QueryUserVO request){
+    @PostMapping(value = "/getList")
+    @PreAuthorize("hasRole('BROWER')")
+    public PageResultVO<UserInfoVO> getList(QueryUserVO request){
         UserInfo info = new UserInfo();
-        info.setLoginAccount(request.getLoginAccount());
-        info.setNickName(request.getNickName());
-        info.setUserName(request.getUserName());
-        List<UserInfo> userInfos = userMapper.getListByPage(request.getCurrentPage(), request.getCurrentPage(), info);
-        if(userInfos.size() > 0){
-            List<UserInfoVO> list = new ArrayList<UserInfoVO>();
-            for(UserInfo userInfo : userInfos){
-                UserInfoVO vo = new UserInfoVO();
-                vo.setCreateTime(DateUtil.toDateFormat(userInfo.getCreateTime()));
-                vo.setModifyTime(DateUtil.toDateTimeFormat(userInfo.getModifyTime()));
-                vo.setOperator(userInfo.getOperator());
-                vo.setUserName(userInfo.getUserName())
-                        .setNickName(userInfo.getNickName())
-                        .setEmail(userInfo.getEmail())
-                        .setId(userInfo.getId())
-                        .setLoginAccount(userInfo.getLoginAccount())
-                        .setMobile(userInfo.getMobile());
-                list.add(vo);
-            }
-            int count = userMapper.getTotalCount(info);
-            return new PageResultVO(list, request.getCurrentPage(), request.getPageSize(), count);
-        }
-        return null;
+        return userService.getUserList(request.getPage(), request.getRows(), info);
     }
 
 
