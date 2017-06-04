fa6ebecc08cf2a3c59e6dad0047db8f36252687a
ydlx002 <469289742@qq.com>
2017-06-04 22:23:45 +0800

    添加security配置

--- src/main/resources/static/js/system/menu.js
+++ src/main/resources/static/js/system/menu.js
@@ -14,18 +14,15 @@ $(document).ready(function(){
     });
 
 
-
-    //
-    // $("#nestable-menu").on("click",function(e){
-    //     var target=$(e.target),action=target.data("action");
-    //     if(action==="expand-all"){
-    //         $(".dd").nestable("expandAll")
-    //     }
-    //     if(action==="collapse-all"){
-    //         $(".dd").nestable("collapseAll")
-    //     }
-    // })
-
+    $("#nestable-menu").on("click",function(e){
+        var target=$(e.target),action=target.data("action");
+        if(action==="expand-all"){
+            $(".dd").nestable("expandAll")
+        }
+        if(action==="collapse-all"){
+            $(".dd").nestable("collapseAll")
+        }
+    })
 
 
     var createMenu = function(obj, menuList){
